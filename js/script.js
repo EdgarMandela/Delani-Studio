1b733e91cc1ca4bfb66fbc8a71d1615fc6e72af6
EdgarMandela <mandela0edgar@gmail.com>
2019-12-09 17:38:42 +0300

    added form function

--- js/script.js
+++ js/script.js
@@ -24,5 +24,42 @@ $("#productIcon").click(function() {
 $(".toggleProductIcon").click(function() {
   $(".toggleProductIcon").hide();
   $("#Product").show();
+})
+$("#pic1").hover(function(){
+  $("#text1").toggle("slow");
+})
+$("#pic2").hover(function(){
+  $("#text2").toggle("slow");
+})
+$("#pic3").hover(function(){
+  $("#text3").toggle("slow");
+})
+$("#pic4").hover(function(){
+  $("#text4").toggle("slow");
 });
+$("#pic5").hover(function(){
+  $("#text5").toggle("slow");
+})
+$("#pic6").hover(function(){
+  $("#text6").toggle("slow");
+})
+$("#pic7").hover(function(){
+  $("#text7").toggle("slow");
+})
+$("#pic8").hover(function(){
+  $("#text8").toggle("slow");
 });
+
+});
+// javascript code//
+function form () {
+  var name=document.getElementByid("mce-NAME nameInput").value;
+  var email=document.getElementById("mce-EMAIL exampleFormControlInput1").value;
+  var text=document.getElementById("mce-MESSAGE messageInput").value;
+
+  if (name == "" ||email== "" || text== "") {
+    alert("please fill out every space.");
+  }else{
+    alert("Hey " + name +" Thank you for contacting us,we will get back to you shortly.");
+  }
+}
