ea0ae9278d2212116bd3c94a289ad1dc7257989d
EdgarMandela <mandela0edgar@gmail.com>
2019-12-09 08:28:40 +0300

    added alert to hover function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
+$("#designIcon").click(function() {
+  $("#Design").hide();
+  $(".toggleDesignIcon").show();
+});
+$(".toggleDesignIcon").click(function() {
+  $(".toggleDesignIcon").hide();
+  $("#Design").show();
+});
+
+$("#devIcon").click(function() {
+  $("#Development").hide();
+  $(".toggleDevIcon").show();
+});
+$(".toggleDevIcon").click(function() {
+  $(".toggleDevIcon").hide();
+  $("#Development").show();
+});
+
+$("#productIcon").click(function() {
+  $("#Product").hide();
+  $(".toggleProductIcon").show();
+});
+$(".toggleProductIcon").click(function() {
+  $(".toggleProductIcon").hide();
+  $("#Product").show();
+
+});
 $(document).ready(function() {
-  $("#designIcon").click(function() {
-    $("#Design").hide();
-    $(".toggleDesignIcon").show();
+  $('.work1Logo').hover(function() {
+    alert('hovering')
+    $("#work1Text").show();
   });
-  $(".toggleDesignIcon").click(function() {
-    $(".toggleDesignIcon").hide();
-    $("#Design").show();
-  });
-
-  $("#devIcon").click(function() {
-    $("#Development").hide();
-    $(".toggleDevIcon").show();
-  });
-  $(".toggleDevIcon").click(function() {
-    $(".toggleDevIcon").hide();
-    $("#Development").show();
-  });
-
-  $("#productIcon").click(function() {
-    $("#Product").hide();
-    $(".toggleProductIcon").show();
-  });
-  $(".toggleProductIcon").click(function() {
-    $(".toggleProductIcon").hide();
-    $("#Product").show();
-
-  });
-  $(document).ready(function() {
-    $('.work1Logo').hover(function() {
-      console.log('hovering')
-      $("#work1Text").show();
-    });
-  });
-
-  
+});
+});
