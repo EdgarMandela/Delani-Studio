1958c4f44db76043bb8216da7014a03aa947a4e6
EdgarMandela <mandela0edgar@gmail.com>
2019-12-09 08:06:57 +0300

    added third row

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@
 .secondRow{
 text-align: center;
 }
+.thirdRow_a {
+  background: url("../assets/backgrounds/s_image.jpg")center,center;
+    text-align: center;
+}
+  .thirdRow_b {
+    width: 100%;
+    height: 300px;
+    margin-right: -20px;
+    margin-left: -20px;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    padding: 0px;
+  }
+  
